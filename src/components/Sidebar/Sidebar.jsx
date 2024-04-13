59c75ea5f71ed02c2e938697a1f7f623166d07e9
Safarmurod999 <supermen0904@gmail.com>
2024-04-13 23:38:35 +0500

    chart and dashboard page added

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom";
 import { adminRoutes } from "../../data/data";
 
 const Sidebar = () => {
+  let username = JSON.parse(localStorage.getItem("username")) || "User";
   return (
     <aside className="flex flex-col w-[350px] h-screen px-5 py-8 overflow-y-auto bg-gray-800 sidebar">
-      <a to="/admin">
+      <a className="mx-auto flex flex-col justify-center cursor-pointer" to="/admin/profile">
         {" "}
         <img
-          className="h-8 w-8"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+          className="h-14 w-14 rounded-full"
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
           alt="Your Company"
         />
+        <span className="text-white text-xl mt-3 text-center capitalize">
+          {username}
+        </span>
       </a>
 
       <div className="flex flex-col justify-between flex-1 mt-6">
--- src/pages/Admin/Dashboard/Dashboard.jsx
+++ src/pages/Admin/Dashboard/Dashboard.jsx
@@ -1,24 +1,149 @@
-import React from 'react'
-import { HiHome } from 'react-icons/hi'
-import { Breadcrumb } from 'flowbite-react'
-const Dashboard = () => {
-  return (
-    <main>
-    <div className="flex-1 py-6">
-      <Breadcrumb aria-label="Orders page" className="ml-[48px] mb-4">
-        <Breadcrumb.Item href="/admin" icon={HiHome}>
-          Dashboard
-        </Breadcrumb.Item>
-        <Breadcrumb.Item href="#"></Breadcrumb.Item>
-      </Breadcrumb>
-      <h1 className="text-3xl font-medium ml-[50px]">Buyurtmalar</h1>
-      <div className="w-full mx-auto px-4 py-6 sm:px-2 lg:px-12">
-        <div className="overflow-x-auto w-full rounded-lg border">
-        </div>
-      </div>
-    </div>
-  </main>
-  )
-}
+import React, { useEffect } from "react";
+import Spinner from "../../../components/Spinner/Spinner";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../../../utils/slice";
+import { HiHome } from "react-icons/hi";
+import { Breadcrumb } from "flowbite-react";
+import { IoLayersSharp } from "react-icons/io5";
+import { MdCalendarMonth } from "react-icons/md";
+import { FaRegCreditCard } from "react-icons/fa";
+import { Line } from "react-chartjs-2";
+import {
+  CategoryScale,
+  LinearScale,
+  Chart,
+  PointElement,
+  LineElement,
+} from "chart.js";
 
-export default Dashboard
+Chart.register(CategoryScale, LinearScale, PointElement, LineElement);
+function groupByDay(items) {
+  const grouped = items.data.map((item) => {
+    const date = new Date(item.createdAt);
+    const key = date.getDate() + "/" + (date.getMonth() + 1);
+    return {
+      count: item.count,
+      date: key,
+      createdAt: item.createdAt,
+    };
+  });
+  return grouped;
+}
+const Dashboard = () => {
+  const dispatch = useDispatch();
+  const orders = useSelector((state) => state.data.data);
+  const isLoading = useSelector((state) => state.data.isLoading);
+  const error = useSelector((state) => state.data.error);
+  useEffect(() => {
+    dispatch(fetchData("orders"));
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Spinner position={"relative"} />;
+  }
+  if (orders) {
+    var groupedByDay = groupByDay(orders);
+    var aggregatedData = groupedByDay.reduce((acc, curr) => {
+      var date = curr.date;
+      if (!acc[date]) {
+        acc[date] = 0;
+      }
+      acc[date] += curr.count;
+      return acc;
+    }, {});
+
+    // Extract dates and counts for chart data
+    var labels = Object.keys(aggregatedData);
+    var counts = Object.values(aggregatedData);
+    console.log(labels, counts);
+    // Data for the chart
+    var chartData = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Count",
+          fill: false,
+          lineTension: 0.4, // Adjust the line tension for curved lines
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: counts,
+        },
+      ],
+    };
+  }
+  if (error) {
+    console.log(error);
+  }
+  return (
+    orders && (
+      <main>
+        <div className="flex-1 py-6">
+          <Breadcrumb aria-label="Orders page" className="ml-[48px] mb-4">
+            <Breadcrumb.Item href="/admin" icon={HiHome}>
+              Dashboard
+            </Breadcrumb.Item>
+            <Breadcrumb.Item href="#"></Breadcrumb.Item>
+          </Breadcrumb>
+          <h1 className="text-3xl font-medium ml-[50px]">Buyurtmalar</h1>
+          <div className="w-full mx-auto px-4 py-6 sm:px-2 lg:px-12">
+            <div className="overflow-x-auto w-full rounded-lg">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="bg-gray-200 p-4 rounded-lg flex flex-col items-center">
+                  <IoLayersSharp className="text-4xl" />
+                  <h1 className="text-3xl font-medium text-center mt-2">
+                    Bugungi
+                  </h1>
+                  <p className="text-center text-gray-600 text-3xl mt-2">
+                    3 400 000 so'm
+                  </p>
+                </div>
+                <div className="bg-gray-200 p-4 rounded-lg flex flex-col items-center">
+                  <MdCalendarMonth className="text-4xl" />
+                  <h1 className="text-3xl font-medium text-center mt-2">
+                    Haftalik
+                  </h1>
+                  <p className="text-center text-gray-600 text-3xl mt-2">
+                    22 300 000 so'm
+                  </p>
+                </div>
+                <div className="bg-gray-200 p-4 rounded-lg flex flex-col items-center">
+                  <FaRegCreditCard className="text-4xl" />
+                  <h1 className="text-3xl font-medium text-center mt-2">
+                    Oylik
+                  </h1>
+                  <p className="text-center text-gray-600 text-3xl mt-2">
+                    334 400 000 so'm
+                  </p>
+                </div>
+              </div>
+              <div className="mt-3 w-1/2">
+                <Line
+                  data={chartData}
+                  options={{
+                    scales: {
+                      y: {
+                        beginAtZero: true,
+                        stepSize: 1,
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  );
+};
+
+export default Dashboard;
